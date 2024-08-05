12b2ca8e8b8707af2d7342a01a0ad4ed44bad53b
Neto test <denis.157911@mail.ru>
2024-08-05 16:15:22 +0400

    Initial commit

---
+++ src/test/java/ru/netology/springbootdemo/SpringBootRestTask1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestTask1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
